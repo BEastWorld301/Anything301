b6f9b40e1a0eba80faaac335d63e25407485eedd
BEastWorld301 <beastworld301@outlook.com>
2024-10-30 01:07:50 +0530

    Update MINEK.bat

--- MINEK.bat
+++ MINEK.bat
@@ -9,6 +9,14 @@ netsh advfirewall firewall set rule group="remote desktop" new enable=yes
 echo IP Address: %COMPUTERNAME%
 echo Port: 3389
 
+:: Display IP address
+echo IP Address:
+ipconfig | findstr /i "IPv4 Address"
+echo.
+
+:: Display Port
+echo Port: 3389
+
 :: Start Remote Desktop Session Host
 net start TermService
 
